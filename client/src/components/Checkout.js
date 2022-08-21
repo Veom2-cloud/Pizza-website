251791ad687e09b9772215b50b79f7b16af365be
Veom2-cloud <veomnohria@gmail.com>
2022-08-21 17:10:14 +0530

    make it better

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import StripeCheckout from "react-stripe-checkout";
+import { placeOrder } from "../actions/orderActions";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
+import Success from "../components/Success";
+export default function Checkout({ subtotal }) {
+  const orderstate = useSelector((state) => state.placeOrderReducer);
+  const { loading, error, success } = orderstate;
+  const dispatch = useDispatch();
+  function tokenHander(token) {
+    console.log(token);
+    dispatch(placeOrder(token, subtotal));
+  }
+
+  return (
+    <div>
+      {loading && <Loading />}
+      {error && <Error error="Something went wrong" />}
+      {success && <Success success="Your Order Placed Successfully" />}
+
+      <StripeCheckout
+        amount={subtotal * 100}
+        shippingAddress
+        token={tokenHander}
+        stripeKey="pk_test_51JXN8ASGX65UtClKQM1qR2CE17v2qSIK3mvU2Mt2nG04vFt2s32pxb7Vj5I8W278pXK92l3D5rZV45XFa1JYgKO900q1RZSIe3"
+        currency="INR"
+      >
+        <button className="btn">Pay Now</button>
+      </StripeCheckout>
+    </div>
+  );
+}
